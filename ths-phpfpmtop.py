8fa1e1f77c229e523b3232184d40e6d55f2986f8
thorvald <thorvald@gridinsoft.com>
2020-03-12 17:40:39 +0200

    first

--- ths-phpfpmtop.py
+++ ths-phpfpmtop.py
@@ -20,9 +20,9 @@ def main():
         except (psutil.NoSuchProcess, psutil.AccessDenied, IndexError):
             pass
     # print(str("{:6s} {:6s} {:6s}").format("Pool name", "VMS", "RSS"))
-    print("%-14s %-15s %s" % ("Pool name", "VMS", "RSS"))
+    print("%-20s %-15s %s" % ("Pool name", "VMS", "RSS"))
     for pool in FullPMemInfo.proc_mem_list.keys():
-        print("%-14s %-15d %d" % (pool, FullPMemInfo.proc_mem_list[pool]['vms'], FullPMemInfo.proc_mem_list[pool]['rss']))
+        print("%-20s %-15d %d" % (pool, FullPMemInfo.proc_mem_list[pool]['vms'], FullPMemInfo.proc_mem_list[pool]['rss']))
         # print(str("{:6s} {:6d} {:6d}").format(pool, FullPMemInfo.proc_mem_list[pool]['vms'], FullPMemInfo.proc_mem_list[pool]['rss']))
 
 if __name__ == "__main__":
