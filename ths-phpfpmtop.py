d8f6d6b68f637cd81495c9815b19aaabc20f169f
thorvald <thorvald@gridinsoft.com>
2020-03-12 12:39:08 +0200

    first

--- ths-phpfpmtop.py
+++ ths-phpfpmtop.py
@@ -6,37 +6,25 @@ import sys
 from PMemInfo import FullPMemInfo
 
 
-# def zzzz():
-#     for prinfo in psutil.process_iter():
-#         try:
-#             cmd_first = prinfo.cmdline()[0]
-#             if re.match('.*php-fpm: pool.+', cmd_first):
-#                 pool = cmd_first.split()[-1]
-#                 p_mem_data = prinfo.memory_info()
-#                 p_mem_rss = p_mem_data.rss
-#                 p_mem_vms = p_mem_data.vms
-#                 FullPMemInfo.p_mem_rss_full(pool, p_mem_rss)
-#                 FullPMemInfo.p_mem_vms_full(pool, p_mem_vms)
-#         except (psutil.NoSuchProcess, psutil.AccessDenied, IndexError):
-#             pass
-#     # line_data = str()
-#     print(FullPMemInfo.proc_mem_list)
+def main():
+    for prinfo in psutil.process_iter():
+        try:
+            cmd_first = prinfo.cmdline()[0]
+            if re.match('.*php-fpm: pool.+', cmd_first):
+                pool = cmd_first.split()[-1]
+                p_mem_data = prinfo.memory_info()
+                p_mem_rss = p_mem_data.rss
+                p_mem_vms = p_mem_data.vms
+                FullPMemInfo.p_mem_rss_full(pool, p_mem_rss)
+                FullPMemInfo.p_mem_vms_full(pool, p_mem_vms)
+        except (psutil.NoSuchProcess, psutil.AccessDenied, IndexError):
+            pass
+    line_data = str()
+    print(FullPMemInfo.proc_mem_list)
 #
 # for key in proc_mem_list.keys():
 #     print(key, proc_mem_list[key]['rss'])
 
-FullPMemInfo = FullPMemInfo()
-for prinfo in psutil.process_iter():
-    try:
-        cmd_first = prinfo.cmdline()[0]
-        if re.match('.*php-fpm: pool.+', cmd_first):
-            pool = cmd_first.split()[-1]
-            p_mem_data = prinfo.memory_info()
-            p_mem_rss = p_mem_data.rss
-            p_mem_vms = p_mem_data.vms
-            FullPMemInfo.p_mem_rss_full(pool, p_mem_rss)
-            FullPMemInfo.p_mem_vms_full(pool, p_mem_vms)
-    except (psutil.NoSuchProcess, psutil.AccessDenied, IndexError):
-        pass#
-# if __name__ == "__main__":
-#     sys.exit(main())
+if __name__ == "__main__":
+    FullPMemInfo = FullPMemInfo()
+    sys.exit(main())
