e2ba869dbf1f7f24cdbfc9cc94e6ed4889ba49cc
thorvaldhrafn <thorvaldr.hrafn@gmail.com>
2020-03-17 12:29:03 +0200

    first

--- ths-phpfpmtop.py
+++ ths-phpfpmtop.py
@@ -16,7 +16,8 @@ def print_l_poolmem(proc_mem_list, srt="rss"):
             print(str("{:"+str(leng_p)+"}" "{:>15d} {:>10d}").format(pool, proc_mem_list[pool]['vms'],
                                                                      proc_mem_list[pool]['rss']))
     if srt == "rss":
-        for pool in sorted(proc_mem_list, key=proc_mem_list.get("rss"), reverse=True):
+        sproc_mem_list = OrderedDict(sorted(proc_mem_list, key=proc_mem_list.get("rss"), reverse=True))
+        for pool in sproc_mem_list:
             print(str("{:"+str(leng_p)+"}" "{:>15d} {:>10d}").format(pool, proc_mem_list[pool]['vms'],
                                                                      proc_mem_list[pool]['rss']))
     # for pool in sproc_mem_list.keys():
