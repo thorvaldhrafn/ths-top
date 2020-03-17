381c7295d1f573365929ff724752ad44de2655c8
thorvaldhrafn <thorvaldr.hrafn@gmail.com>
2020-03-17 14:38:02 +0200

    first

--- ths-phpfpmtop.py
+++ ths-phpfpmtop.py
@@ -10,7 +10,7 @@ from PMemInfo import FullPMemInfo
 
 def bytes_conv(m_data, t_data):
     if t_data == "kbytes":
-        pass
+        m_data = float(m_data)
     if t_data == "mbytes":
         m_data = m_data / 1024
         print(type(m_data))
@@ -26,21 +26,21 @@ def print_l_poolmem(proc_mem_list, srt="rss", t_data="mbytes"):
     if srt == "name":
         sproc_mem_list = OrderedDict(sorted(proc_mem_list.items()))
         for pool in sproc_mem_list:
-            print(str("{:" + str(leng_p) + "}" "{:>15d} {:>10d}").format(pool,
+            print(str("{:" + str(leng_p) + "}" "{:>15f} {:>10f}").format(pool,
                                                                          bytes_conv(proc_mem_list[pool]['vms'], t_data),
                                                                          bytes_conv(proc_mem_list[pool]['rss'],
                                                                                     t_data)))
     if srt == "rss":
         sproc_mem_list = OrderedDict(sorted(proc_mem_list.items(), key=lambda x: getitem(x[1], 'rss'), reverse=True))
         for pool in sproc_mem_list:
-            print(str("{:" + str(leng_p) + "}" "{:>15d} {:>10d}").format(pool, bytes_conv(proc_mem_list[pool]['vms'],
+            print(str("{:" + str(leng_p) + "}" "{:>15f} {:>10f}").format(pool, bytes_conv(proc_mem_list[pool]['vms'],
                                                                                           t_data),
                                                                          bytes_conv(proc_mem_list[pool]['rss'],
                                                                                     t_data)))
     if srt == "vms":
         sproc_mem_list = OrderedDict(sorted(proc_mem_list.items(), key=lambda x: getitem(x[1], 'vms'), reverse=True))
         for pool in sproc_mem_list:
-            print(str("{:" + str(leng_p) + "}" "{:>15d} {:>10d}").format(pool, bytes_conv(proc_mem_list[pool]['vms'],
+            print(str("{:" + str(leng_p) + "}" "{:>15f} {:>10f}").format(pool, bytes_conv(proc_mem_list[pool]['vms'],
                                                                                           t_data),
                                                                          bytes_conv(proc_mem_list[pool]['rss'],
                                                                                     t_data)))
