a4c7f6a371dd983d0b7d10918d7d410e686f1e47
thorvaldhrafn <thorvaldr.hrafn@gmail.com>
2020-03-18 19:10:04 +0200

    first

--- ths-phpfpmtop.py
+++ ths-phpfpmtop.py
@@ -29,7 +29,7 @@ def prnt_line(leng_p, pool, proc_mem_list, t_data):
     return str(p_line + "\n")
 
 
-def showscr(proc_mem_list, srt="rss", t_data="mbytes"):
+def showscr(srt="rss", t_data="mbytes"):
     try:
         stdscr = curses.initscr()
         curses.noecho()
@@ -97,21 +97,21 @@ def p_data():
 
 
 def main():
-    for prinfo in psutil.process_iter():
-        try:
-            with prinfo.oneshot():
-                cmd_lst = prinfo.cmdline()
-                cmd_str = ' '.join(map(str, cmd_lst))
-                if re.match('.*php-fpm: pool .*', cmd_str):
-                    pool = cmd_str.split()[-1]
-                    p_mem_data = prinfo.memory_info()
-                    p_mem_rss = p_mem_data.rss
-                    p_mem_vms = p_mem_data.vms
-                    FullPMemInfo.p_mem_rss_full(pool, p_mem_rss)
-                    FullPMemInfo.p_mem_vms_full(pool, p_mem_vms)
-        except (psutil.NoSuchProcess, psutil.AccessDenied, IndexError):
-            pass
-    showscr(FullPMemInfo.proc_mem_list, "rss", "mbytes")
+    # for prinfo in psutil.process_iter():
+    #     try:
+    #         with prinfo.oneshot():
+    #             cmd_lst = prinfo.cmdline()
+    #             cmd_str = ' '.join(map(str, cmd_lst))
+    #             if re.match('.*php-fpm: pool .*', cmd_str):
+    #                 pool = cmd_str.split()[-1]
+    #                 p_mem_data = prinfo.memory_info()
+    #                 p_mem_rss = p_mem_data.rss
+    #                 p_mem_vms = p_mem_data.vms
+    #                 FullPMemInfo.p_mem_rss_full(pool, p_mem_rss)
+    #                 FullPMemInfo.p_mem_vms_full(pool, p_mem_vms)
+    #     except (psutil.NoSuchProcess, psutil.AccessDenied, IndexError):
+    #         pass
+    showscr("rss", "mbytes")
 
 
 if __name__ == "__main__":
