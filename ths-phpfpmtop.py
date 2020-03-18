88a3ec59ce8ddd0e60ab6cfb49072c50cb55a0ac
thorvaldhrafn <thorvaldr.hrafn@gmail.com>
2020-03-18 14:35:24 +0200

    first

--- ths-phpfpmtop.py
+++ ths-phpfpmtop.py
@@ -51,17 +51,16 @@ def print_l_poolmem(proc_mem_list, srt="rss", t_data="mbytes"):
 def main():
     for prinfo in psutil.process_iter():
         try:
-            cmd_lst = prinfo.cmdline()
-            cmd_str = ' '.join(map(str, cmd_lst))
-            if re.match('.*php-fpm: pool .*', cmd_str):
-                pool = cmd_str.split()[-1]
-                p_fmem_data = prinfo.memory_full_info()
-                print(cmd_str, p_fmem_data.swap)
-                p_mem_data = prinfo.memory_info()
-                p_mem_rss = p_mem_data.rss
-                p_mem_vms = p_mem_data.vms
-                FullPMemInfo.p_mem_rss_full(pool, p_mem_rss)
-                FullPMemInfo.p_mem_vms_full(pool, p_mem_vms)
+            with prinfo.oneshot():
+                cmd_lst = prinfo.cmdline()
+                cmd_str = ' '.join(map(str, cmd_lst))
+                if re.match('.*php-fpm: pool .*', cmd_str):
+                    pool = cmd_str.split()[-1]
+                    p_mem_data = prinfo.memory_info()
+                    p_mem_rss = p_mem_data.rss
+                    p_mem_vms = p_mem_data.vms
+                    FullPMemInfo.p_mem_rss_full(pool, p_mem_rss)
+                    FullPMemInfo.p_mem_vms_full(pool, p_mem_vms)
         except (psutil.NoSuchProcess, psutil.AccessDenied, IndexError):
             pass
     print_l_poolmem(FullPMemInfo.proc_mem_list, "rss", "mbytes")
