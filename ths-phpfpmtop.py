0f900f2c6d538864f12e9ff5e6c785604b04bd23
thorvaldhrafn <thorvaldr.hrafn@gmail.com>
2020-03-19 12:29:03 +0200

    first

--- ths-phpfpmtop.py
+++ ths-phpfpmtop.py
@@ -31,25 +31,25 @@ def prnt_line(leng_p, pool, proc_mem_list, t_data):
 
 def showscr(srt="rss", t_data="mbytes"):
     try:
-        stdscr = curses.initscr()
-        curses.noecho()
-        curses.cbreak()
-        stdscr.keypad(1)
-        proc_mem_list = p_data()
-        leng_p = len(max(proc_mem_list.keys(), key=len))
-        leng_p += 6
-        stdscr.addstr(1, 1, str("{:" + str(leng_p) + "}" "{:<15s} {:<10s}").format("Pool name", "VMS", "RSS\n\n"), curses.A_BOLD)
-        l_num = 2
-        sproc_mem_list = OrderedDict(sorted(proc_mem_list.items(), key=lambda x: getitem(x[1], 'rss'), reverse=True))
-        for pool in sproc_mem_list:
-            stdscr.addstr(l_num, 1, prnt_line(leng_p, pool, proc_mem_list, t_data), curses.A_NORMAL)
-            l_num += 1
-        stdscr.refresh()
-
         while True:
+            stdscr = curses.initscr()
+            curses.noecho()
+            curses.cbreak()
+            stdscr.keypad(1)
+            proc_mem_list = p_data()
+            leng_p = len(max(proc_mem_list.keys(), key=len))
+            leng_p += 6
+            stdscr.addstr(1, 1, str("{:" + str(leng_p) + "}" "{:<15s} {:<10s}").format("Pool name", "VMS", "RSS\n\n"), curses.A_BOLD)
+            l_num = 2
+            sproc_mem_list = OrderedDict(sorted(proc_mem_list.items(), key=lambda x: getitem(x[1], 'rss'), reverse=True))
+            for pool in sproc_mem_list:
+                stdscr.addstr(l_num, 1, prnt_line(leng_p, pool, proc_mem_list, t_data), curses.A_NORMAL)
+                l_num += 1
+
             ch = stdscr.getch()
             if ch == ord('q'):
                 break
+            stdscr.refresh()
     except:
         traceback.print_exc()
     finally:
