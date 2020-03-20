d9d76aa370091f81353dd4c0ae6161b11f4ff7ba
thorvald <thorvald@gridinsoft.com>
2020-03-20 13:25:03 +0200

    first

--- ths-phpfpmtop.py
+++ ths-phpfpmtop.py
@@ -39,8 +39,8 @@ def showscr(srt="rss", t_data="mbytes"):
             proc_mem_list = p_data()
             leng_p = len(max(proc_mem_list.keys(), key=len))
             leng_p += 6
-            stdscr.addstr(1, 1, str("{:" + str(leng_p) + "}" "{:<15s} {:<10s}").format("Pool name", "VMS", "RSS"), curses.A_REVERSE)
-            l_num = 2
+            stdscr.addstr(0, 1, str("{:" + str(leng_p) + "}" "{:<15s} {:<10s}").format("Pool name", "VMS", "RSS"), curses.A_REVERSE)
+            l_num = 1
             sproc_mem_list = OrderedDict(sorted(proc_mem_list.items(), key=lambda x: getitem(x[1], 'rss'), reverse=True))
             for pool in sproc_mem_list:
                 stdscr.addstr(l_num, 1, prnt_line(leng_p, pool, proc_mem_list, t_data), curses.A_NORMAL)
